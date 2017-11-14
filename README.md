d145632fc568bb8ef92f4269da9f4084fb9209c9
Daniele Salvatore <danieleslvtr@gmail.com>
2017-11-14 17:21:49 +0100

    Clear folders

---
+++ README.md
@@ -0,0 +1,16 @@
+# Compare image
+
+## Prerequisites
+
+- ```brew install pkg-config cairo libpng jpeg giflib```
+- and eventually https://www.npmjs.com/package/canvas#installation
+
+## Steps
+
+- https://www.browserstack.com/screenshots
+- Select devices and insert URL
+- Download final zip archive
+- Unzip the archive in the ./screenshots folder
+- in index.js define the master image
+- run with ```node .```
+- browse the output folder
--- index.js
+++ index.js
@@ -4,28 +4,59 @@ const SCREENSHOT_FOLDER = "./screenshots/";
 const OUTPUT_FOLDER = "./output/";
 const MASTER = "win10_chrome_50.0.jpg";
 
-fs.readdirSync(SCREENSHOT_FOLDER)
-    .forEach(file => {
+const cleanFolders = () => {
+    fs.readdirSync(SCREENSHOT_FOLDER)
+        .forEach(file => {
 
-        // skip not .jpg, master file and .keepme
-    if (!file.endsWith(".jpg") || file === ".keepme" || file === ".MASTER") {
-        console.log("Abort: ", file);
-        return;
-    }
+            // skip not .keepme
+            if (file === ".keepme") {
+                return;
+            }
 
-    console.log("Process:", file)
+            fs.unlinkSync(path)
 
-    // The parameters can be Node Buffers
-    // data is the same as usual with an additional getBuffer() function
-    compareImages(
-        fs.readFileSync(SCREENSHOT_FOLDER + MASTER),
-        fs.readFileSync(SCREENSHOT_FOLDER + file),
-    ).then(function (data) {
-        const output = OUTPUT_FOLDER + file
-        fs.writeFileSync(output, data.getBuffer());
-        console.log("Stored:", output)
-    }, (error) => {
-        console.log(error)
-    });
-});
+        });
 
+    fs.readdirSync(OUTPUT_FOLDER)
+        .forEach(file => {
+
+            // skip not .keepme
+            if (file === ".keepme") {
+                return;
+            }
+
+            fs.unlinkSync(path)
+
+        });
+};
+
+const init = () => {
+    fs.readdirSync(SCREENSHOT_FOLDER)
+        .forEach(file => {
+
+            // skip not .jpg, master file and .keepme
+            if (!file.endsWith(".jpg") || file === ".keepme" || file === ".MASTER") {
+                console.log("Abort: ", file);
+                return;
+            }
+
+            console.log("Process:", file);
+
+            // The parameters can be Node Buffers
+            // data is the same as usual with an additional getBuffer() function
+            compareImages(
+                fs.readFileSync(SCREENSHOT_FOLDER + MASTER),
+                fs.readFileSync(SCREENSHOT_FOLDER + file),
+            ).then(function (data) {
+                const output = OUTPUT_FOLDER + file;
+                fs.writeFileSync(output, data.getBuffer());
+                console.log("Stored:", output)
+            }, (error) => {
+                console.log(error)
+            });
+        });
+};
+
+cleanFolders();
+
+init();
